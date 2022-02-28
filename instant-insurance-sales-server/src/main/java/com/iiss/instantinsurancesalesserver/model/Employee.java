35826ddd2d5e5b42e9a36f114cc9790daddb20f0
Dete-Desai <detedesai@gmail.com>
2022-02-28 18:15:34 +0300

    employee table

---
+++ instant-insurance-sales-server/src/main/java/com/iiss/instantinsurancesalesserver/model/Employee.java
@@ -0,0 +1,37 @@
+package com.iiss.instantinsurancesalesserver.model;
+
+import javax.persistence.Id;
+
+public class Employee {
+
+    @Id
+    public int id;
+    public String firstName;
+    public String lastName;
+
+    public Employee() {}
+
+    public int getId(){
+        return id;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
