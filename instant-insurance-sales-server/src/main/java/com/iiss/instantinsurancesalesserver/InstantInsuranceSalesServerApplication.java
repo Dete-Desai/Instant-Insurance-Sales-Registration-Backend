55459ad2fc970637ee5bd0fbd0e60e326f3edeaf
Lamech Dete <lamech.dete@smart.local>
2022-02-28 14:26:48 +0300

    3rd commit

---
+++ instant-insurance-sales-server/src/main/java/com/iiss/instantinsurancesalesserver/InstantInsuranceSalesServerApplication.java
@@ -0,0 +1,13 @@
+package com.iiss.instantinsurancesalesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantInsuranceSalesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantInsuranceSalesServerApplication.class, args);
+	}
+
+}
---
+++ instant-insurance-sales-server/src/test/java/com/iiss/instantinsurancesalesserver/InstantInsuranceSalesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiss.instantinsurancesalesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantInsuranceSalesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
