daff60027a7ced7fd118bfdabd4c8270f1e2dd69
Dete-Desai <detedesai@gmail.com>
2022-02-28 10:08:26 +0300

    Initial Commit

---
+++ Instant-insurance-sales-registration-backend/src/test/java/com/iiss/Instantinsurancesalesregistrationbackend/InstantInsuranceSalesRegistrationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiss.Instantinsurancesalesregistrationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantInsuranceSalesRegistrationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
